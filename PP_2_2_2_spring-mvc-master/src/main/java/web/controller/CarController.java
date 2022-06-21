6443c207221eedeb7780253d5bf47ae20955b8d3
Alexander <105540044+sotnicoff@users.noreply.github.com>
2022-06-21 10:25:01 +0300

    удалил проверку на число и null

--- PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
@@ -19,20 +19,12 @@ public class CarController {
 
     @GetMapping("/cars")
     public String index(Model model) {
-        if (carDAO.index() == null) {
-            throw new NullPointerException("Not data exists!");
-        }
         model.addAttribute("cars", carDAO.index());
         return "index";
     }
 
     @GetMapping("/cars/{count}")
     public String showCars(@PathVariable int count, Model model) {
-        if (count < 0) {
-            throw new RuntimeException("Pathing variable must be positive");
-        } else if (count > carDAO.index().size()) {
-            throw new RuntimeException("Pathing variable is too big");
-        }
         model.addAttribute("cars", carDAO.showCars(count));
         return "index";
     }
